061d6ec28562998c667fbf48dbe5a442fbd40058
tarun <tarunkumar8059@gmail.com>
2023-04-03 12:22:04 +0530

    upggmeta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   <Head>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-<meta name="google-site-verification" content="BtqUqNIwx2IH-1jz2bNNBEC26oUYy22GsSfNdqWfLj0" />
+  <meta name="google-site-verification" content="AbBSUdH4hKhXW68-cnEnKLVss2r-tJ8kP0g6asVMu3U" />
   </Head>
   <Navbar/>
   <Component {...pageProps} />
