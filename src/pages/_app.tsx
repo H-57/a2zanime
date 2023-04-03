4c049bb136ad080f3b15d15804f1d33141d76f06
tarun <tarunkumar8059@gmail.com>
2023-04-03 12:18:41 +0530

    add google meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   <Head>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-
+<meta name="google-site-verification" content="BtqUqNIwx2IH-1jz2bNNBEC26oUYy22GsSfNdqWfLj0" />
   </Head>
   <Navbar/>
   <Component {...pageProps} />
