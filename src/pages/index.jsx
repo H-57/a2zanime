bde4e09bb2d81e7d1f4d525b7ffa72a56d6d1037
tarun <tarunkumar8059@gmail.com>
2023-06-01 13:54:53 +0530

    test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -30,18 +30,18 @@ export default function Home({series,movies}) {
       <h2 style={{width:"100vw"}} className="neonheading">Series</h2>
                 {series.map((elem, index) => {
                   
-                  return <Link key={index} href={elem.url}> 
+                  return <li key={index} ><Link href={elem.url}> 
                   <SeasonCards   title={elem.title} image={elem.image}  />
-                  </Link>
+                  </Link></li>
                 })}
 
 <h2 style={{width:"100vw"}} className="neonheading">Movies</h2>
 
 {movies.map((elem, index) => {
                   
-                  return <Link key={index} href={elem.url}> 
+                  return  <li key={index} ><Link  href={elem.url}> 
                   <SeasonCards   title={elem.title} image={elem.image}  />
-                  </Link>
+                  </Link></li>
                 })}
 
               </ul>
--- src/pages/p/[name]/index.jsx
+++ src/pages/p/[name]/index.jsx
@@ -1,98 +1,80 @@
 import SeasonCards from "../../../components/seasonCards";
 import Link from "next/link";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import Head from "next/head";
 
-
 export async function getStaticPaths() {
- 
-    
-  let data = await (
-    await fetch(`${process.env.API_URL}/anime/.json`)).json();      //get data for exists all animes                                     
-  data=Object.keys(data)
-  
-  const paths = data.map((elem) => {                //return present anime pages
+  let data = await (await fetch(`${process.env.API_URL}/anime/.json`)).json(); //get data for exists all animes
+  data = Object.keys(data);
+
+  const paths = data.map((elem) => {
+    //return present anime pages
     return {
       params: {
-        name:(elem).toString(),
+        name: elem.toString(),
       },
     };
   });
   return {
-     
     paths,
-    
-    fallback: false, 
 
-  }
+    fallback: false,
+  };
 }
 
 export const getStaticProps = async (context) => {
-    const name=context.params.name
-    console.log(name)
-      let res = await fetch(
-        `${process.env.API_URL}/anime/${name}/season.json` //get data for present animes and thir seasons and show all season cards
-      );
-      const data = await res.json();
-      return {
-        props: {
-          data,
-        },
-      };
-    };
-    
-    function AnimeName({data}) {
-      const router = useRouter()
-     
+  const name = context.params.name;
+  console.log(name);
+  let res = await fetch(
+    `${process.env.API_URL}/anime/${name}/season.json` //get data for present animes and thir seasons and show all season cards
+  );
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
+};
 
-          
-        
-        
-          return (
-            <>
-           <Head>
-           <title>Azanime - Best Source For Hindi Anime &amp; Cartoons</title>
+function AnimeName({ data }) {
+  const router = useRouter();
 
-<meta name="description" content="Best Source For Hindi Anime &amp; Cartoons" />
+  return (
+    <>
+      <Head>
+        <title>Azanime - Best Source For Hindi Anime &amp; Cartoons</title>
+
+        <meta
+          name="description"
+          content="Best Source For Hindi Anime &amp; Cartoons"
+        />
         <meta name="keywords" content="all anime videos,video,pokemon" />
-        
       </Head>
-             
-              <ul id="video" className="video">
-                {data.map((elem, index) => {
-                  
-                  return <li key={index}><Link  href={`/p/${router.query.name}/season${index+1}`}> 
-                  <SeasonCards   title={elem.title} image={elem.image}  />
-                  </Link>
-                  </li>
-                })}
-              </ul>
-             <style jsx>{`
-        
-        
-        /* for ul list store all video cards */
-       
-        
-      
-           
-        .video {
-        
-            display: flex;
-            flex-wrap: wrap;
-          
-            row-gap: 20px;
-            column-gap: 40px;
-        }
-        
-        
-        
-        `}</style>
-            </>
+
+      <ul id="video" className="video">
+        {data.map((elem, index) => {
+          return (
+            
+              <Link key={index} href={`/p/${router.query.name}/season${index + 1}`}>
+                <SeasonCards title={elem.title} image={elem.image} />
+              </Link>
+            
           );
-        
-        
-        
-        
+        })}
+      </ul>
+      <style jsx>{`
+        /* for ul list store all video cards */
+
+        .video {
+          display: flex;
+          flex-wrap: wrap;
+
+          row-gap: 20px;
+          column-gap: 40px;
         }
-        
-        export default AnimeName;
+      `}</style>
+    </>
+  );
+}
+
+export default AnimeName;
